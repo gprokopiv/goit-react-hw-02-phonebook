14e2ba46816bcddda37dab83d07c5161a0233b27
Prokopiv <halyna.kotlyar@gmail.com>
2021-03-29 00:11:28 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 // import Dropdown from './components/Dropdown';
 // import ColorPicker from './components/ColorPicker';
-import TodoList from './components/TodoList';
+// import TodoList from './components/TodoList';
 import initialTodos from './todos.json';
+import Container from './components/Container'
 
 // const colorPickerOptions = [
 //   { label: 'red', color: '#F44336' },
@@ -14,41 +15,89 @@ import initialTodos from './todos.json';
 //   { label: 'indigo', color: '#3F51B5' },
 // ];
 
+// class App extends Component {
+//   state = {
+//     todos: initialTodos,
+//   };
+
+//   deleteTodo = todoId => {
+//     this.setState(prevState => ({
+//       todos: prevState.todos.filter(todo => todo.id !== todoId),
+//     }));
+//   };
+
+//   render() {
+//     const { todos } = this.state;
+
+//     const totalTodoCount = todos.length;
+//     const completedTodoCount = todos.reduce(
+//       (total, todo) => (todo.completed ? total + 1 : total),
+//       0,
+//     );
+
+//     return (
+//       <>
+//         <h1>Состояние компонента</h1>
+
+//         <Counter initialValue={10} />
+//         {/* <Dropdown /> */}
+//         {/* <ColorPicker options={colorPickerOptions} /> */}
+
+//         <div>
+//           <p>Общее кол-во: {totalTodoCount}</p>
+//           <p>Кол-во выполненных: {completedTodoCount}</p>
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
 class App extends Component {
-  state = {
-    todos: initialTodos,
-  };
-
-  deleteTodo = todoId => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== todoId),
-    }));
-  };
-
-  render() {
-    const { todos } = this.state;
-
-    const totalTodoCount = todos.length;
-    const completedTodoCount = todos.reduce(
-      (total, todo) => (todo.completed ? total + 1 : total),
-      0,
-    );
-
-    return (
-      <>
-        <h1>Состояние компонента</h1>
-
-        {/* <Counter initialValue={10} /> */}
-        {/* <Dropdown /> */}
-        {/* <ColorPicker options={colorPickerOptions} /> */}
-
-        <div>
-          <p>Общее кол-во: {totalTodoCount}</p>
-          <p>Кол-во выполненных: {completedTodoCount}</p>
-        </div>
-      </>
-    );
-  }
+state = {
+  todos: initialTodos,
+  name: '',
+  tag: '',
 }
+handleChange = event => {
+
+  const { name, value } = event.currentTarget;
+  this.setState({      [name]: value
+
+  })
+}
+handleSubmit = e => {
+  e.preventDefault();
+  console.log(this.state);
+}
+
+render() {
+
+
+  return(
+    <Container>
+<form onSubmit={this.handleSubmit}>
+  <label>
+    name <input type='text' name='name' value={this.state.name} onChange={this.handleChange}></input>
+  </label>
+  <label>
+    surname <input type='text' name='tag' value={this.state.tag} onChange={this.handleChange}></input>
+  </label>
+  <button type='submit' >
+    Send
+  </button>
+</form>
+
+
+
+    </Container>
+
+  )
+}
+
+
+}
+
+
 
 export default App;
---
+++ src/components/Container/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Container = ({ children }) => {
+    return (
+        <div>
+          {children}  
+        </div>
+    );
+};
+
+
+export default Container;
